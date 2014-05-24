c2769516dc3925d604a5a3c9eda6a661413c8bf1
adriangonzy <adrian.gonzy@gmail.com>
2014-05-24 22:34:58 +0200

    ran gofmt

--- main.go
+++ main.go
@@ -1,46 +1,46 @@
 package dashboard
 
 import (
-		"fmt"
-        "net/http"
-        "regexp"
+	"fmt"
+	"net/http"
+	"regexp"
 
- 		"appengine"
-    	"appengine/urlfetch"
+	"appengine"
+	"appengine/urlfetch"
 
-		"github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func init() {
-        http.HandleFunc("/ws", root)
+	http.HandleFunc("/ws", root)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
 
 	var resp *http.Response
 	var doc *goquery.Document
-    var e error
+	var e error
 
-    url := "http://www.homelidays.com/hebergement/p6640918"
-    c := appengine.NewContext(r)
-	
+	url := "http://www.homelidays.com/hebergement/p6640918"
+	c := appengine.NewContext(r)
+
 	if resp, e = urlfetch.Client(c).Get(url); e != nil {
 		http.Error(w, e.Error(), http.StatusInternalServerError)
-        return
+		return
 	}
 
-    if doc, e = goquery.NewDocumentFromResponse(resp); e != nil {
-    	c.Debugf("%v", e)
-    }
+	if doc, e = goquery.NewDocumentFromResponse(resp); e != nil {
+		c.Debugf("%v", e)
+	}
 
-    jsondata := regexp.MustCompile("{\".*}}")
+	jsondata := regexp.MustCompile("{\".*}}")
 
-    // Parse first body script text
-    data := doc.Find("body > script").First().Text()
-    loc := jsondata.FindAllString(data, 3)
-    c.Debugf("%v", len(loc))
-    if (len(loc) == 0) {
-    	return
-    }
-	fmt.Fprintf(w, "{ \"reserved\":" + loc[0] + ", \"minweek\":" + loc[1] + ", \"unit\":" + loc[2] + "}")
+	// Parse first body script text
+	data := doc.Find("body > script").First().Text()
+	loc := jsondata.FindAllString(data, 3)
+	c.Debugf("%v", len(loc))
+	if len(loc) == 0 {
+		return
+	}
+	fmt.Fprintf(w, "{ \"reserved\":"+loc[0]+", \"minweek\":"+loc[1]+", \"unit\":"+loc[2]+"}")
 }
