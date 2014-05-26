207485c0218fdff1830bf2c3504c78533a0c84a7
adriangonzy <adrian.gonzy@gmail.com>
2014-05-26 23:29:52 +0200

    better formating

--- main.go
+++ main.go
@@ -4,24 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
-	"regexp"
 	"errors"
+	"regexp"
 
 	"encoding/json"
 
 	"appengine"
+	"appengine/datastore"
 	"appengine/urlfetch"
-    "appengine/datastore"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type Apart struct {
-	data string
 	Reserved []byte `json:",string"`
-	Data []byte `json:",string"`
-	UnitId string 
-	Name string
+	Data     []byte `json:",string"`
+	UnitId   string
+	Name     string
 }
 
 func init() {
@@ -32,42 +31,42 @@ func apartRouter(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
 	switch r.Method {
-	   case "POST":
-	       // Serve the resource.
-	   case "GET":
-	   		// scrap apart data
-	   		apart, e := newApart(r, c)
+	case "POST":
+		// Serve the resource.
+	case "GET":
+		// scrap apart data
+		apart, e := newApart(r, c)
 
-	   		if e != nil {	
-	   			http.Error(w, e.Error(), http.StatusInternalServerError)
-	   		}
-			
-			// add apart to datastore
-			key := datastore.NewKey(c, "Apart", apart.Name, 0, nil)
-	   		_, e = datastore.Put(c, key, &apart)
-	   		if e != nil {
-	   			http.Error(w, e.Error(), http.StatusInternalServerError)
-	   		}
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
 
-	   		var test Apart
+		// add apart to datastore
+		key := datastore.NewKey(c, "Apart", apart.Name, 0, nil)
+		_, e = datastore.Put(c, key, &apart)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
 
-			if err := datastore.Get(c, key, &test); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
+		var test Apart
 
-			res, e := json.Marshal(test)
-			if e != nil {
-				http.Error(w, e.Error(), http.StatusInternalServerError)
-			}
+		if err := datastore.Get(c, key, &test); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-			fmt.Fprint(w, string(res))
+		res, e := json.Marshal(test)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+		}
 
-	   case "PUT":
-	       // Update an existing record.
-	   case "DELETE":
-	       // Remove the record.
-	   default:
-	       // Give an error message.
+		fmt.Fprint(w, string(res))
+
+	case "PUT":
+		// Update an existing record.
+	case "DELETE":
+		// Remove the record.
+	default:
+		// Give an error message.
 	}
 }
 
@@ -83,20 +82,20 @@ func newApart(r *http.Request, c appengine.Context) (Apart, error) {
 	}
 
 	data, e := scrapApartData(id, c)
-	if 	e != nil {
+	if e != nil {
 		c.Errorf("%v", e)
 		return emptyApart, e
 	}
 
-	if  len(data) == 0 {
+	if len(data) == 0 {
 		return emptyApart, errors.New("No data was found for " + id)
 	}
 
 	return Apart{
 		Reserved: []byte(data[0]),
-		Data: []byte(data[2]),
-		UnitId: getUnitId(data[2]),
-		Name: id,
+		Data:     []byte(data[2]),
+		UnitId:   getUnitId(data[2]),
+		Name:     id,
 	}, nil
 }
 
@@ -104,12 +103,12 @@ const HOMELIDAYS_APART_URL = "http://www.homelidays.com/hebergement/p"
 
 func getUnitId(encodedData string) string {
 	var data map[string]interface{}
-		
-    if err := json.Unmarshal([]byte(encodedData), &data); err != nil {
-        panic(err)
-    }
 
-    return data["unitId"].(string)
+	if err := json.Unmarshal([]byte(encodedData), &data); err != nil {
+		panic(err)
+	}
+
+	return data["unitId"].(string)
 }
 
 func scrapApartData(id string, c appengine.Context) ([]string, error) {
